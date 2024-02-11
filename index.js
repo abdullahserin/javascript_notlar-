a8cfae51c21f620ca5d030dac60d3d72d1d560f3
Abdullah SERIN <abdullahserin@Abdullah-MacBook-Air.local>
2024-02-12 00:54:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//variable 1-var 2-let 3-const
+
+/*let Name = "abdullah";
+let Surname = "Serin";
+console.log(Name + "" + Surname);
+*/
+
+
+//type of ıse bıze hangı tur degısken gonderdıgını acıklar.
+// suan guncel olan javascript dılınde var degılde let kullanılır.
+//const degıskenı degıstırelemeyen degısken olarak kullanılır.
+//node.js yukledıgınde ıstersen termınalde ıstersen de tarayıcı console uzerınde kontrol(cıktıları kontrol  edebılırsın)
+
+/*let number1 = "20";
+let number2 = "10";
+
+let numberAdd = number1 + number2;
+console.log(numberAdd);
+*/
+
+//string ifadelerde ise sayıyı aynen alıp dırek yazar bunun ıcınde number yazarak ınteger degerıne cevırebılırız.
+
+let number1 = "20";
+let number2 = "10";
+
+let numberAdd = Number(number1) + Number(number2);
+console.log(numberAdd);
+
+
+
